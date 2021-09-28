b0ea984dabd4d55c3ea0b4e29785bf98a29940bb
Tim Marchant <fromtimwithcode@gmail.com>
2021-09-28 00:14:05 -0500

    search bar styling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import TodoList from './components/TodoList';
 import TodoForm from './components/TodoForm';
 import './App.css'
 import styled from 'styled-components';
+import { HiOutlineSearchCircle } from 'react-icons/hi';
 
 const tasks = [
   {
@@ -65,27 +66,77 @@ class App extends React.Component {
     })
   }
 
+  handleSearch = () => {
+    console.log('test')
+  }
+
   render() {
-    return (
+    return (<>
+      <Nav>
+        <TitleDiv>
+          <StyledH1>todoizt</StyledH1>
+        </TitleDiv>
+        <SearchDiv>
+          <Search />
+          <StyledButton onClick={this.handleSearch}>
+            <HiOutlineSearchCircle color={'#BB86FC'} size={'2rem'} />
+          </StyledButton>
+        </SearchDiv>
+      </Nav>
       <StyledDiv>
-        <StyledH1>Todoizt</StyledH1>
         <TodoList tasks={this.state.tasks} handleToggle={this.handleToggle} /> 
         <TodoForm handleAdd={this.handleAdd} handleClear={this.handleClear} />
       </StyledDiv>
-    );
+    </>);
   }
 }
+
+const Nav = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding-left: 1rem;
+  padding-right: 0.5rem;
+  margin-top: 0.5rem;
+`
+const TitleDiv = styled.div`
+  justify-content: flex-start;
+`
+
+const StyledH1 = styled.h1`
+  font-size: 3rem;
+  margin-bottom: 1rem;
+  color: #BB86FC;
+`
+
+const SearchDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: flex-start;
+  width: 100%;
+  margin-top: .5rem;
+`
+
+const Search = styled.input`
+  background: #40375c;
+  border: none;
+  height: 2rem;
+  border-radius: .2rem;
+  color: #fff;
+  width: 60%;
+`
+
+const StyledButton = styled.button`
+  background: none;
+  border: none;
+`
 
 const StyledDiv = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   margin-top: 2rem;
-`
-
-const StyledH1 = styled.h1`
-  font-size: 3rem;
-  margin-bottom: 1rem;
 `
 
 export default App;
